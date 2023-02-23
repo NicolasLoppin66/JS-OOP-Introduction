2f78f86a680e2be2214585f530bf947e3949db8b
NicolasLoppin66 <loppin.n66@gmail.com>
2023-02-23 12:21:23 +0100

    Update : Intro

---
+++ prototype.js
@@ -0,0 +1,61 @@
+"use strict"; // Activation du mode strict pour plus d'info sur les erreurs de structuration
+console.log('prototype playground');
+
+// On va crée cet objet sous forme de prototype
+// const car = {
+//     id: 0,
+//     label: '',
+//     brand_id: 0,
+//     brand: {
+//         id: 0,
+//         label: '',
+//     },
+//     price: 0.00,
+//     getTaxePrice: function () {
+//         return this.price * 1.2
+//     }
+// };
+
+// Définition du constructeur du prototype
+const Brand = function (jsonData) {
+    this.id = jsonData.id;
+    this.label = jsonData.label;
+};
+
+// Définition d'une méthode liée a une instance (instance = objet crée par new Brand()) (PHP: $unObjet->getGhettoLabel())
+Brand.prototype.getGhettoLabel = function () {
+    return this.label + ' wesh !';
+};
+
+const pegeotBrand = new Brand({ id: 2, label: 'Peugeot' });
+
+console.log(pegeotBrand.getGhettoLabel());
+
+// Prototype de car
+const Car = function (jsonData) {
+    this.id = jsonData.id;
+    this.label = jsonData.label;
+    this.brand_id = jsonData.brand_id;
+
+    this.brand = new Brand(jsonData.brand);
+
+    this.price = jsonData.price;
+}
+
+// Définition d'une méthode liée a une instance
+Car.prototype.getTaxePrice = function () {
+    return this.price * 1.2;
+};
+
+const carPeugeot = new Car({
+    id: 5,
+    label: 'Peugoet 2008 GT a vendre',
+    brand_id: 5,
+    brand: {
+        id: 5,
+        label: 'Peugoet',
+    },
+    price: 15000.00,
+})
+
+console.dir(carPeugeot);
